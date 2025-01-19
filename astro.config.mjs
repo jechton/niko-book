57812ded7ff821fdd68b9401bd021f6fce497e4c
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-01-18 20:07:41 -0500

    perf: add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,10 @@ import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://twinkings.fun",
-  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  integrations: [mdx(), sitemap(), tailwind(), icon(), partytown()],
 });
