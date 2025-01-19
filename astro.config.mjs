e5d7cb036f13c53cf0dfd17577d0bca3a21f0e8b
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-01-18 20:14:26 -0500

    feat: use vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
@@ -9,8 +9,12 @@ import icon from "astro-icon";
 
 import partytown from "@astrojs/partytown";
 
+import vercelStatic from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://twinkings.fun",
   integrations: [mdx(), sitemap(), tailwind(), icon(), partytown()],
+  output: "static",
+  adapter: vercelStatic({ imageService: true }),
 });
