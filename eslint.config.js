09df1698a6dc64a9105ed7e09663e9ac80f42bcb
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-02-20 21:43:42 -0500

    ci: add husky, better prettier and eslint config, stylelint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,22 @@
 import pluginJs from "@eslint/js";
 import eslintPluginAstro from "eslint-plugin-astro";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  // add more generic rule sets here, such as:
+  { files: ["**/*.{js,mjs,cjs,ts,tsx,astro}"] },
+  { ignores: [".husky/", ".astro/", "dist/"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
+    ...jsxA11y.flatConfigs.recommended,
     rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
+      ...jsxA11y.flatConfigs.recommended.rules,
+      "jsx-a11y/label-has-associated-control": "off",
     },
   },
 ];
