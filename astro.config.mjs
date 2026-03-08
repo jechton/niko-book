ce31466d84f638834afd8e355b781a97937f1db9
Jeremiah <44993244+jechton@users.noreply.github.com>
2026-03-08 16:11:52 -0400

    build: use lefthook instead of husky and lint-staged

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,24 @@
 // @ts-check
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
+import vercelStatic from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
-
-import partytown from "@astrojs/partytown";
-
-import vercelStatic from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  site: "https://twinkings.fun",
-  integrations: [mdx(), sitemap(), icon(), partytown()],
-  output: "static",
-  adapter: vercelStatic({
-    webAnalytics: {
-      enabled: true,
-    },
-    imageService: true,
-  }),
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	site: "https://twinkings.fun",
+	integrations: [mdx(), sitemap(), icon(), partytown()],
+	output: "static",
+	adapter: vercelStatic({
+		webAnalytics: {
+			enabled: true,
+		},
+		imageService: true,
+	}),
 });
